4d5b7ab44deb57a159f1957e7ee91abe93131396
Leo Gau <leo.gau1@gmail.com>
2015-07-01 21:23:22 -0700

    Update for heroku

--- server.js
+++ server.js
@@ -16,7 +16,15 @@ app.get('/(:id)', function(req, res) {
 })
 
 var sharejs = require('share');
-require('redis');
+var redisClient;
+console.log(process.env.REDISTOGO_URL);
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+  redisClient.auth(rtg.auth.split(":")[1]);
+} else {
+  redisClient = require("redis").createClient();
+}
 
 var shareOptions = {
   db: {type: 'redis'}
